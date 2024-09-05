12118a7f6368c27b84befaf56320f242c8f4ec6b
kcwang <kcwang.work@gmail.com>
2024-09-05 15:31:05 +0800

    建立 Models 並使用 InMemory Db

--- TeaTime.Api/Program.cs
+++ TeaTime.Api/Program.cs
@@ -1,6 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using TeaTime.Api.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<TeaTimeContext>(opt => opt.UseInMemoryDatabase("TeaTimeDb"));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
