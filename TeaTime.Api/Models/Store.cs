8535c9f950bc2c93c97cfabda3c722db286752bb
az-Strawman <az12109tw@yahoo.com.tw>
2024-09-06 11:31:40 +0800

    OK

--- TeaTime.Api/Models/Store.cs
+++ TeaTime.Api/Models/Store.cs
@@ -1,7 +1,12 @@
-﻿namespace TeaTime.Api.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace TeaTime.Api.Models
 {
     public class Store
     {
+        [JsonIgnore]
         public long Id { get; set; }
 
         public string Name { get; set; } = string.Empty;
@@ -9,5 +14,9 @@
         public string PhoneNumber { get; set; } = string.Empty;
 
         public string MenuUrl { get; set; } = string.Empty;
+
+
+
+
     }
 }
